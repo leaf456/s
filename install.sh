bbe74c228953c281bff61df9ab288ddda964eeb5
linuxhublinuxhub-dev <55876116+linuxhublinuxhub-dev@users.noreply.github.com>
2020-09-12 08:50:54 +1000

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo su
-touch /home/administrator/desktop/Welcome.txt
-echo "Thankyou for using St Gregs OS. This Os is based on Ubuntu 20.04." > /home/administrator/desktop/Welcome.txt
-apt-get install chromium-browser
-gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/warty-final-ubuntu.png"
